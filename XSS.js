ddd45322c655e1c88fe77e1e25a83a707ea63bba
JDL-84 <James.lynes@googlemail.com>
2020-08-28 21:18:19 +0100

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -4,4 +4,6 @@ var websession  = '1';
 var mysessionno  = '1';
 
 //Set LastRefresh
-var LastRefresh  = Date.now();
+var CurrDT          = new Date();
+var TimeAsMS        = CurrDT.getTime();
+var LastRefresh = TimeAsMS;
