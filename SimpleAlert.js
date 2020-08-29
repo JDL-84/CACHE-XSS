c348168ecbf884b6fd250dcc5185bf162f60ee39
JDL-84 <James.lynes@googlemail.com>
2020-08-29 14:43:31 +0100

    Update SimpleAlert.js

--- SimpleAlert.js
+++ SimpleAlert.js
@@ -1,3 +1,11 @@
-//Authoer: JDL
-//<script src="https://jdl-84.github.io/CACHE-XSS/SimpleAlert.js"></script> 
+/**
+ * Summary. Simple XSS Test
+ *
+ * Description. Used to test calling external JavaScript via XSS.
+ *
+ * @author JDL-84
+ * @link https://jdl-84.github.io/CACHE-XSS/SimpleAlert.js
+ */
+
+//<script src='https://jdl-84.github.io/CACHE-XSS/SimpleAlert.js'/>
 alert("TEST");
